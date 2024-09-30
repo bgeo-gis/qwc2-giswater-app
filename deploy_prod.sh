64f0374cb4d5ab07e26fa83cdcbcda99e7a56f41
BgeoDevTeam <serveradmin@bgeo.es>
2024-09-30 14:47:57 +0000

    fix: correct deploy scripts

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,4 +1,4 @@
 yarn run prod
-rm -rf /home/bgeoadmin/qwc-docker-dev/qwc-docker/volumes/qwc2/* 
-cp -R /home/bgeoadmin/qwc2-demo-app/prod/* /home/bgeoadmin/qwc-docker-dev/qwc-docker/volumes/qwc2/
+rm -rf /home/bgeoadmin/qwc-docker-dev/qwc-docker/volumes/qwc2/*
+cp -R /home/bgeoadmin/qwc2-giswater-app/prod/* /home/bgeoadmin/qwc-docker-dev/qwc-docker/volumes/qwc2/
 chown -R 33:33 /home/bgeoadmin/qwc-docker-dev/qwc-docker/volumes/qwc2/assets
