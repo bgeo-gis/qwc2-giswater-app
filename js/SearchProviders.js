f69ebf5d67c0635db38a423abc333217e39d9715
BgeoDevTeam <serveradmin@bgeo.es>
2024-11-13 15:47:01 +0000

    bug: added missing imports

--- js/SearchProviders.js
+++ js/SearchProviders.js
@@ -10,6 +10,8 @@
 import yaml from 'js-yaml';
 import CoordinatesUtils from '../qwc2/utils/CoordinatesUtils';
 import IdentifyUtils from '../qwc2/utils/IdentifyUtils';
+import GwUtils from 'qwc2-giswater/utils/GwUtils';
+import isEmpty from 'lodash.isempty';
 
 function coordinatesSearch(text, searchParams, callback) {
     const displaycrs = searchParams.displaycrs || "EPSG:4326";
