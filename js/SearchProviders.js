2d186a9b2c682d16601b5ca6e4bc9c7eebe70f63
BgeoDevTeam <serveradmin@bgeo.es>
2025-02-05 13:35:45 +0000

    fix(searchproviders): update searchprovider definition after searchbox rework

--- js/SearchProviders.js
+++ js/SearchProviders.js
@@ -6,274 +6,11 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-
 import yaml from 'js-yaml';
-import CoordinatesUtils from '../qwc2/utils/CoordinatesUtils';
-import IdentifyUtils from '../qwc2/utils/IdentifyUtils';
+import axios from 'axios';
 import GwUtils from 'qwc2-giswater/utils/GwUtils';
 import isEmpty from 'lodash.isempty';
-
-function coordinatesSearch(text, searchParams, callback) {
-    const displaycrs = searchParams.displaycrs || "EPSG:4326";
-    const matches = text.match(/^\s*([+-]?\d+\.?\d*)[,\s]\s*([+-]?\d+\.?\d*)\s*$/);
-    const items = [];
-    if (matches && matches.length >= 3) {
-        const x = parseFloat(matches[1]);
-        const y = parseFloat(matches[2]);
-        if (displaycrs !== "EPSG:4326") {
-            items.push({
-                id: "coord0",
-                text: x + ", " + y + " (" + displaycrs + ")",
-                x: x,
-                y: y,
-                crs: displaycrs,
-                bbox: [x, y, x, y]
-            });
-        }
-        if (x >= -180 && x <= 180 && y >= -90 && y <= 90) {
-            const title = Math.abs(x) + (x >= 0 ? "°E" : "°W") + ", "
-                      + Math.abs(y) + (y >= 0 ? "°N" : "°S");
-            items.push({
-                id: "coord" + items.length,
-                text: title,
-                x: x,
-                y: y,
-                crs: "EPSG:4326",
-                bbox: [x, y, x, y]
-            });
-        }
-        if (x >= -90 && x <= 90 && y >= -180 && y <= 180 && x !== y) {
-            const title = Math.abs(y) + (y >= 0 ? "°E" : "°W") + ", "
-                      + Math.abs(x) + (x >= 0 ? "°N" : "°S");
-            items.push({
-                id: "coord" + items.length,
-                text: title,
-                x: y,
-                y: x,
-                crs: "EPSG:4326",
-                bbox: [y, x, y, x]
-            });
-        }
-    }
-    const results = [];
-    if (items.length > 0) {
-        results.push(
-            {
-                id: "coords",
-                titlemsgid: "search.coordinates",
-                items: items
-            }
-        );
-    }
-    callback({results: results});
-}
-
-/** ************************************************************************ **/
-
-class NominatimSearch {
-    static TRANSLATIONS = {};
-
-    static search(text, searchParams, callback, axios) {
-        const viewboxParams = {};
-        if (searchParams.filterBBox) {
-            viewboxParams.viewbox = CoordinatesUtils.reprojectBbox(searchParams.filterBBox, searchParams.mapcrs, "EPSG:4326").join(",");
-            viewboxParams.bounded = 1;
-        }
-        axios.get("https://nominatim.openstreetmap.org/search", {params: {
-            'q': text,
-            'addressdetails': 1,
-            'polygon_geojson': 1,
-            'limit': 20,
-            'format': 'json',
-            'accept-language': searchParams.lang,
-            ...viewboxParams,
-            ...(searchParams.cfgParams || {})
-        }}).then(response => {
-            const locale = searchParams.lang;
-            if (NominatimSearch.TRANSLATIONS[locale] === undefined) {
-                NominatimSearch.TRANSLATIONS[locale] = {promise: NominatimSearch.loadLocale(locale, axios)};
-                NominatimSearch.TRANSLATIONS[locale].promise.then(() => {
-                    NominatimSearch.parseResults(response.data, NominatimSearch.TRANSLATIONS[locale].strings, callback);
-                });
-            } else if (NominatimSearch.TRANSLATIONS[locale].promise) {
-                NominatimSearch.TRANSLATIONS[locale].promise.then(() => {
-                    NominatimSearch.parseResults(response.data, NominatimSearch.TRANSLATIONS[locale].strings, callback);
-                });
-            } else if (NominatimSearch.TRANSLATIONS[locale].strings) {
-                NominatimSearch.parseResults(response.data, NominatimSearch.TRANSLATIONS[locale].strings, callback);
-            }
-        });
-    }
-    static parseResults(obj, translations, callback) {
-        const results = [];
-        const groups = {};
-        let groupcounter = 0;
-
-        (obj || []).map(entry => {
-            if (!(entry.class in groups)) {
-                let title = entry.type;
-                try {
-                    title = translations[entry.class][entry.type];
-                } catch (e) {
-                    /* pass */
-                }
-                groups[entry.class] = {
-                    id: "nominatimgroup" + (groupcounter++),
-                    // capitalize class
-                    title: title,
-                    items: []
-                };
-                results.push(groups[entry.class]);
-            }
-
-            // shorten display_name
-            let text = entry.display_name.split(', ').slice(0, 3).join(', ');
-            // map label
-            const label = text;
-
-            // collect address fields
-            const address = [];
-            if (entry.address.town) {
-                address.push(entry.address.town);
-            }
-            if (entry.address.city) {
-                address.push(entry.address.city);
-            }
-            if (entry.address.state) {
-                address.push(entry.address.state);
-            }
-            if (entry.address.country) {
-                address.push(entry.address.country);
-            }
-            if (address.length > 0) {
-                text += "<br/><i>" + address.join(', ') + "</i>";
-            }
-
-            // reorder coords from [miny, maxy, minx, maxx] to [minx, miny, maxx, maxy]
-            const b = entry.boundingbox.map(coord => parseFloat(coord));
-            const bbox = [b[2], b[0], b[3], b[1]];
-
-            groups[entry.class].items.push({
-                id: entry.place_id,
-                // shorten display_name
-                text: text,
-                label: label,
-                bbox: bbox,
-                geometry: entry.geojson,
-                x: 0.5 * (bbox[0] + bbox[2]),
-                y: 0.5 * (bbox[1] + bbox[3]),
-                crs: "EPSG:4326",
-                provider: "nominatim"
-            });
-        });
-        callback({results: results});
-    }
-    static loadLocale(locale, axios) {
-        return new Promise((resolve) => {
-            axios.get('https://raw.githubusercontent.com/openstreetmap/openstreetmap-website/master/config/locales/' + locale + '.yml')
-                .then(resp2 => {
-                    NominatimSearch.TRANSLATIONS[locale] = {strings: NominatimSearch.parseLocale(resp2.data, locale)};
-                    resolve(true);
-                }).catch(() => {
-                    NominatimSearch.TRANSLATIONS[locale] = {
-                        promise: axios.get('https://raw.githubusercontent.com/openstreetmap/openstreetmap-website/master/config/locales/' + locale.slice(0, 2) + '.yml')
-                            .then(resp3 => {
-                                NominatimSearch.TRANSLATIONS[locale] = {strings: NominatimSearch.parseLocale(resp3.data, locale.slice(0, 2))};
-                                resolve(true);
-                            }).catch(() => {
-                                NominatimSearch.TRANSLATIONS[locale] = {strings: {}};
-                                resolve(true);
-                            })
-                    };
-                });
-        });
-    }
-    static parseLocale(data, locale) {
-        const doc = yaml.load(data, {json: true});
-        try {
-            return doc[locale].geocoder.search_osm_nominatim.prefix;
-        } catch (e) {
-            return {};
-        }
-    }
-}
-
-/** ************************************************************************ **/
-
-class QgisSearch {
-
-    static search(text, searchParams, callback, axios) {
-
-        const filter = {...searchParams.cfgParams.expression};
-        const values = {TEXT: text};
-        const params = {
-            SERVICE: 'WMS',
-            VERSION: searchParams.theme.version,
-            REQUEST: 'GetFeatureInfo',
-            CRS: searchParams.theme.mapCrs,
-            WIDTH: 100,
-            HEIGHT: 100,
-            LAYERS: [],
-            FILTER: [],
-            WITH_MAPTIP: false,
-            WITH_GEOMETRY: true,
-            feature_count: searchParams.cfgParams.featureCount || 100,
-            info_format: 'text/xml'
-        };
-        Object.keys(filter).forEach(layer => {
-            Object.entries(values).forEach(([key, value]) => {
-                filter[layer] = filter[layer].replaceAll(`$${key}$`, value.replace("'", "\\'"));
-            });
-            params.LAYERS.push(layer);
-            params.FILTER.push(layer + ":" + filter[layer]);
-        });
-        params.QUERY_LAYERS = params.LAYERS = params.LAYERS.join(",");
-        params.FILTER = params.FILTER.join(";");
-        axios.get(searchParams.theme.featureInfoUrl, {params}).then(response => {
-            callback(QgisSearch.searchResults(
-                IdentifyUtils.parseResponse(response.data, searchParams.theme, 'text/xml', null, searchParams.mapcrs),
-                searchParams.cfgParams.title, searchParams.cfgParams.resultTitle
-            ));
-        }).catch(() => {
-            callback({results: []});
-        });
-    }
-    static searchResults(features, title, resultTitle) {
-        const results = [];
-        Object.entries(features).forEach(([layername, layerfeatures]) => {
-            const items = layerfeatures.map(feature => {
-                const values = {
-                    ...feature.properties,
-                    id: feature.id,
-                    layername: layername
-                };
-                return {
-                    id: "qgis." + layername + "." + feature.id,
-                    text: resultTitle ? resultTitle.replace(/{([^}]+)}/g, match => values[match.slice(1, -1)]) : feature.displayname,
-                    x: 0.5 * (feature.bbox[0] + feature.bbox[2]),
-                    y: 0.5 * (feature.bbox[1] + feature.bbox[3]),
-                    crs: feature.crs,
-                    bbox: feature.bbox,
-                    geometry: feature.geometry
-                };
-            });
-            results.push(
-                {
-                    id: "qgis." + layername,
-                    title: title + ": " + layername,
-                    items: items
-                }
-            );
-        });
-        return {results};
-    }
-    static getResultGeometry(resultItem, callback) {
-        callback({geometry: resultItem.geometry, crs: resultItem.crs});
-    }
-}
-
-
-/** ************************************************************************ **/
+import VectorLayerUtils from 'qwc2/utils/VectorLayerUtils';
 
 function giswaterSearch(text, searchParams, callback, axios) {
     const requestUrl = GwUtils.getServiceUrl("search");
@@ -297,6 +34,10 @@ function giswaterSearch(text, searchParams, callback, axios) {
             const output = [];
             result.data.forEach((group) => {
                 const items = [];
+                //Don't include tab address
+                if (group.section === "basic_search_v2_tab_address"){
+                    return;
+                }
                 group.values?.forEach((entry) => {
                     items.push({
                         id: entry.value,
@@ -329,9 +70,14 @@ function giswaterSearch(text, searchParams, callback, axios) {
     }
 }
 
-function giswaterGetGeometry(result, callback, axios) {
-    console.log("geom result", result);
+function giswaterGetGeometry(result, callback, customAxios) {
     const requestUrl = GwUtils.getServiceUrl("search");
+    const axiosInstance = customAxios || axios;
+
+    if (!axiosInstance) {
+        console.error("Error: axios not defined in giswaterGetGeometry.");
+        return;
+    }
     if (!isEmpty(requestUrl)) {
         const props = result.props;
         const extras = `"value": "${props.display_name}", "section": "${props.section}", "filterKey": "${props.key}", "filterValue": "${props.value}", "execFunc": "${props.execFunc}", "tableName": "${props.tableName}", "searchAdd": "${props.searchAdd}"`;
@@ -339,21 +85,49 @@ function giswaterGetGeometry(result, callback, axios) {
             theme: props.theme,
             extras: extras
         };
-        axios.get(requestUrl + "setsearch", { params: params }).then(response => {
+        axiosInstance.get(requestUrl + "setsearch", { params: params }).then(response => {
             const result = response.data;
+
+            if (!result.data.geometry || !result.data.geometry.st_astext) {
+                console.error("Error: invalid geometry:", result.data.geometry);
+                return;
+            }
+
+            const geoJsonFeature = VectorLayerUtils.wktToGeoJSON(
+                result.data.geometry.st_astext,
+                props.crs,
+                props.crs
+            );
+
+            if (!geoJsonFeature) {
+                console.error("Error converting geometry to GeoJSON.");
+                return;
+            }
+
+            const center = VectorLayerUtils.getFeatureCenter(geoJsonFeature);
+            const bbox = VectorLayerUtils.computeFeatureBBox(geoJsonFeature);
+
+
+
             callback({
                 geometry: result.data.geometry.st_astext,
-                crs: props.crs
+                crs: props.crs,
+                center: center,
+                bbox: bbox
             });
+
+            console.log("Callback::: ", callback);
         }).catch((e) => {
-            console.error(e);
+            console.error("Error calling setsearch: ", e);
         });
+
     }
 }
 
 /** ************************************************************************ **/
 
 function customSearch(text, searchParams, callback, axios) {
+    console.log("CUSTOM SEARCH::::::::::::")
     const requestUrl = GwUtils.getServiceUrl("customSearch");
 
     const tables = searchParams.cfgParams.tables;
@@ -397,7 +171,14 @@ function customSearch(text, searchParams, callback, axios) {
     });
 }
 
-function customSearchGeom(resultItem, callback, axios) {
+function customSearchGeom(resultItem, callback, customAxios) {
+    const axiosInstance = customAxios || axios;
+    console.log("axios:", axiosInstance);
+
+    if (!axiosInstance) {
+        console.error("Error: axios not defined in customSearchGeom.");
+        return;
+    }
     const requestUrl = GwUtils.getServiceUrl("customSearch");
 
     const params = {
@@ -405,31 +186,14 @@ function customSearchGeom(resultItem, callback, axios) {
         searchtable: resultItem.props.searchtable,
         displaytext: resultItem.text
     };
-    axios.get(requestUrl + "searchGeom", { params: params }).then(response => {
+    axiosInstance.get(requestUrl + "searchGeom", { params: params }).then(response => {
         callback({ geometry: response.data, crs: resultItem.crs });
     });
 }
 
 /** ************************************************************************ **/
 
-
-export const SearchProviders = {
-    coordinates: {
-        labelmsgid: "search.coordinates",
-        onSearch: coordinatesSearch,
-        handlesGeomFilter: false
-    },
-    nominatim: {
-        label: "OpenStreetMap",
-        onSearch: NominatimSearch.search,
-        handlesGeomFilter: false
-    },
-    qgis: {
-        label: "QGIS",
-        onSearch: QgisSearch.search,
-        getResultGeometry: QgisSearch.getResultGeometry,
-        handlesGeomFilter: false
-    },
+window.QWC2SearchProviders = {
     giswater: {
         label: "Giswater",
         handlesGeomFilter: true,
@@ -442,3 +206,5 @@ export const SearchProviders = {
         getResultGeometry: customSearchGeom
     }
 };
+
+console.log("SearchProviders loaded:", window.QWC2SearchProviders);
