635aec113056d9ae8fbb8063adb38e9caf987fb4
BgeoDevTeam <serveradmin@bgeo.es>
2025-04-01 07:51:10 +0000

    fix: revert last commit mispell

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,4 +1,4 @@
 yarn run prod
-rm -rf /opt/qwc2-focker/qwc-docker/volumes/qwc2/*
-cp -R /opt/qwc2-giswater-app/prod/* /opt/qwc2-focker/qwc-docker/volumes/qwc2/
-chown -R 33:33 /opt/qwc2-focker/qwc-docker/volumes/qwc2/assets
+rm -rf /opt/qwc2-docker/qwc-docker/volumes/qwc2/*
+cp -R /opt/qwc2-giswater-app/prod/* /opt/qwc2-docker/qwc-docker/volumes/qwc2/
+chown -R 33:33 /opt/qwc2-docker/qwc-docker/volumes/qwc2/assets
